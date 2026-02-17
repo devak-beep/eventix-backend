a10075648797a418da29ff8ec6bfce556f29f5be
Devak <223csd11@svitvasad.ac.in>
2026-02-17 11:19:35 +0530

    Add Vercel serverless configuration

---
+++ index.js
@@ -0,0 +1,15 @@
+require("dotenv").config();
+const app = require("./src/app");
+const connectDB = require("./src/config/db");
+
+let isConnected = false;
+
+const handler = async (req, res) => {
+  if (!isConnected) {
+    await connectDB();
+    isConnected = true;
+  }
+  return app(req, res);
+};
+
+module.exports = handler;
