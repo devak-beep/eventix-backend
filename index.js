806b0266d7d05f9ffc0eb68c868fc5e4aaad69fb
Devak <223csd11@svitvasad.ac.in>
2026-03-05 11:48:55 +0530

    Add database connection to serverless handler

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 
 let app = null;
+let dbConnected = false;
 
 module.exports = async (req, res) => {
   console.log(`[VERCEL] ${req.method} ${req.url}`);
@@ -15,6 +16,16 @@ module.exports = async (req, res) => {
   }
 
   try {
+    // Connect to database once
+    if (!dbConnected) {
+      console.log("[VERCEL] Connecting to database...");
+      const connectDB = require("./src/config/db");
+      await connectDB();
+      dbConnected = true;
+      console.log("[VERCEL] Database connected");
+    }
+
+    // Load app once
     if (!app) {
       console.log("[VERCEL] Loading app...");
       app = require("./src/app");
